bd40aaa2f1c71707b1ccf6ba0feff1e52b81e38a
Alexander <alexander-b00t@users.noreply.github.com>
2021-08-04 10:51:12 +0300

    check git work-test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import Card from './components/Card';
-import Drawer from './components/Drawer';
 import Header from './components/Header';
+import Drawer from './components/Drawer';
+
+const arr = [
+  {
+    title: 'Мужские кроссовки Nike Blazer Mid Suede',
+    price: '11000',
+    imageUrl: '/img/sneakers/1.jpg'
+  },
+  {
+    title: 'Мужские Кроссовки Nike Air Max 270',
+    price: '12000',
+    imageUrl: '/img/sneakers/2.jpg'
+  },
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: '13000',
+    imageUrl: '/img/sneakers/3.jpg'
+  },
+  {
+    title: 'Кроссовки Puma X Aka Boku Future Rider',
+    price: '14000',
+    imageUrl: '/img/sneakers/4.jpg'
+  },
+];
+
 function App() {
   return (
     <div className="wrapper clear">
-      <Drawer />
-      <Header />
+      <Drawer/>
+      <Header/>
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
-          <h1> Все кросcовки</h1>
+          <h1> Все кроcовки</h1>
           <div className="search-block d-flex">
-            <img src="/img/search.svg" alt="Search" />
-            <input placeholder="Поиск.... " />
+            <img src="/img/search.svg" alt="Search"/>
+            <input placeholder="Поиск.... "/>
           </div>
         </div>
 
         <div className="d-flex">
-          <Card />
-          <Card />
-          <Card />
+
+          {arr.map((obj) => (
+            <Card
+              title={obj.title}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+            />
+          ))}
         </div>
+
       </div>
     </div> /* wrapper */
   );
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,46 +1,40 @@
 function Drawer() {
   return (
-    <div style={{ display: 'none' }} className="overlay">
+    <div style={{display: 'none'}} className="overlay">
       <div className="drawer">
         <h2 className="d-flex justify-between mb-30">
           Корзина
-          <img className="cu-p" src="/img/btn-remove.svg" alt="Remove" />
+          <img className="cu-p" src="/img/btn-remove.svg" alt="Remove"/>
         </h2>
         <div className="items">
           <div className="cartItem d-flex align-center mb-20">
-            <div
-              style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
-              className="cartItemImg"></div>
 
+            <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemImg"></div>
             <div className="mr-20 flex">
               <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
               <b>12 999 руб.</b>
             </div>
-            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"/>
           </div>
 
           <div className="cartItem d-flex align-center mb-20">
-            <div
-              style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
-              className="cartItemImg"></div>
+            <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemImg"></div>
 
             <div className="mr-20 flex">
               <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
               <b>12 999 руб.</b>
             </div>
-            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"/>
           </div>
 
           <div className="cartItem d-flex align-center mb-20">
-            <div
-              style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }}
-              className="cartItemImg"></div>
+            <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemImg"></div>
 
             <div className="mr-20 flex">
               <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
               <b>12 999 руб.</b>
             </div>
-            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"/>
           </div>
         </div>
 
@@ -58,7 +52,7 @@ function Drawer() {
             </li>
           </ul>
           <button className="greenButton">
-            Оформить заказ <img src="/img/arrow.svg" alt="Arrow" />
+            Оформить заказ <img src="/img/arrow.svg" alt="Arrow"/>
           </button>
         </div>
       </div>
